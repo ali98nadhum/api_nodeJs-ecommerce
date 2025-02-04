8c2ee9d94d3f119f710f2906fbb29fef3375e7c5
Ali nadhum <alinadhum1998@gmail.com>
2025-02-05 00:45:46 +0300

    add updateCategoryValidator

--- utils/vaildators/CategoryVaildators.js
+++ utils/vaildators/CategoryVaildators.js
@@ -3,7 +3,7 @@ const VaildatorMiddleware = require("../../middlewares/vaildatorMiddleware");
 
 exports.createCategoryValidator = [
     check("title")
-    .notEmpty().withMessage("title is requireddddddddddd")
+    .notEmpty().withMessage("title is required")
     .isLength({min:5}).withMessage("Too short category title")
     .isLength({max:100}).withMessage("Too long category title"),
     VaildatorMiddleware,
@@ -13,4 +13,14 @@ exports.createCategoryValidator = [
 exports.deleteCategoryValidator = [
     check("id").isMongoId().withMessage("Invalid category id"),
     VaildatorMiddleware
+]
+
+exports.updateCategoryValidator = [
+    check("id")
+    .isMongoId()
+    .withMessage("Invalid category id"),
+    check("title")
+    .isLength({min:5}).withMessage("Too short category title")
+    .isLength({max:100}).withMessage("Too long category title"),
+    VaildatorMiddleware,
 ]
