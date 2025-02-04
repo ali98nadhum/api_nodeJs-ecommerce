6ce3ebbc821bb35c5aef8d894fee8332555a1dea
Ali nadhum <alinadhum1998@gmail.com>
2025-02-04 23:05:48 +0300

    add getAllCategory route

--- routes/CategoryRoute.js
+++ routes/CategoryRoute.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
-const { createCategory } = require("../controllers/categoryController");
+const { createCategory, getAllCategories } = require("../controllers/categoryController");
 const uploadPhoto = require("../middlewares/uploadPhoto");
 const {createCategoryValidator} = require("../utils/vaildators/CategoryVaildators")
 
-router.route("/").post(uploadPhoto.single("image"),createCategoryValidator ,createCategory)
+router.route("/").
+post(uploadPhoto.single("image"),createCategoryValidator ,createCategory)
+.get(getAllCategories)
 
 
 module.exports = router;
