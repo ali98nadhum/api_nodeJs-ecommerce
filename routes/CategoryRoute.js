44a9199e3b0d58b37eef5d4d533827c30da90e32
Ali nadhum <alinadhum1998@gmail.com>
2025-02-06 16:28:02 +0300

    revactor Category route

--- routes/CategoryRoute.js
+++ routes/CategoryRoute.js
@@ -1,15 +1,26 @@
 const router = require("express").Router();
-const { createCategory, getAllCategories, deleteCategory, updateCategory, getCategoryById } = require("../controllers/categoryController");
+const {
+  createCategory,
+  getAllCategories,
+  deleteCategory,
+  updateCategory,
+  getCategoryById,
+} = require("../controllers/categoryController");
 const uploadPhoto = require("../middlewares/uploadPhoto");
-const {createCategoryValidator , deleteCategoryValidator, updateCategoryValidator, getOneCategoryValidator} = require("../utils/vaildators/CategoryVaildators")
+const {
+  createCategoryValidator,
+  deleteCategoryValidator,
+  updateCategoryValidator,
+  getOneCategoryValidator,
+} = require("../utils/vaildators/CategoryVaildators");
 
 router.route("/")
-.post(uploadPhoto.single("image"),createCategoryValidator ,createCategory)
-.get(getAllCategories)
+  .post(uploadPhoto.single("image"), createCategoryValidator, createCategory)
+  .get(getAllCategories);
 
 router.route("/:id")
-.get(getOneCategoryValidator , getCategoryById)
-.delete(deleteCategoryValidator,deleteCategory)
-.put(uploadPhoto.single("image"),updateCategoryValidator ,updateCategory)
+  .get(getOneCategoryValidator, getCategoryById)
+  .delete(deleteCategoryValidator, deleteCategory)
+  .put(uploadPhoto.single("image"), updateCategoryValidator, updateCategory);
 
 module.exports = router;
