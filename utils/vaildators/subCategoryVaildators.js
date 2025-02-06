aedcf68190ba82ccaa187986693b00a5171dc6ee
Ali nadhum <alinadhum1998@gmail.com>
2025-02-06 03:36:17 +0300

    add updateSubCategoryValidator

--- utils/vaildators/subCategoryVaildators.js
+++ utils/vaildators/subCategoryVaildators.js
@@ -18,3 +18,15 @@ exports.deleteSubCategoryValidator = [
     check("id").isMongoId().withMessage("Invalid subcategory id"),
     VaildatorMiddleware,
 ]
+
+
+exports.updateSubCategoryValidator = [
+    check("title")
+    .notEmpty().withMessage("title is required")
+    .isLength({min:2}).withMessage("Too short subcategory title")
+    .isLength({max:100}).withMessage("Too long subcategory title"),
+    check("category")
+    .notEmpty().withMessage("category is required")
+    .isMongoId().withMessage("Invalid category id"),
+    VaildatorMiddleware,
+]
