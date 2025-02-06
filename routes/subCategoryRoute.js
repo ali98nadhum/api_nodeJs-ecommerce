9a4f23041ee9528f52f5e8fde7be56c5dacbef3e
Ali nadhum <alinadhum1998@gmail.com>
2025-02-06 03:34:08 +0300

    add deleteSubCategory route

--- routes/subCategoryRoute.js
+++ routes/subCategoryRoute.js
@@ -1,11 +1,14 @@
-const { createSubCategory } = require("../controllers/subCategoryController");
-const { createSubCategoryValidator } = require("../utils/vaildators/subCategoryVaildators");
+const { createSubCategory, deleteSubCategory } = require("../controllers/subCategoryController");
+const { createSubCategoryValidator, deleteSubCategoryValidator } = require("../utils/vaildators/subCategoryVaildators");
 
 const router = require("express").Router();
 
 
 router.route("/")
 .post(createSubCategoryValidator , createSubCategory)
+
+router.route("/:id")
+.delete(deleteSubCategoryValidator , deleteSubCategory)
 
 
 module.exports = router;
