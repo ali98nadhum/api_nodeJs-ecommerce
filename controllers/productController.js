fa2f0ef3e06a0435284beb69952481a81cdef44d
Ali nadhum <alinadhum1998@gmail.com>
2025-02-09 18:24:54 +0300

    add deleteProduct fun

--- controllers/productController.js
+++ controllers/productController.js
@@ -34,3 +34,20 @@ module.exports.createProduct = asyncHandler(async (req, res) => {
   const product = await ProductModel.create(req.body);
   res.status(201).json({ data: product });
 });
+
+
+
+// ==================================
+// @desc Delete a product
+// @route /api/v1/products/:id
+// @method POST
+// @access private (only admin)
+// ==================================
+module.exports.deleteProduct = asyncHandler(async(req , res) => {
+  const product = await ProductModel.findByIdAndDelete(req.params.id);
+  if(!product){
+    return res.status(404).json({message: 'Product not found'})
+  }
+
+  res.status(200).json({message: 'Product deleted successfully'})
+})
