d317bdd25dee758c92915ad805ff1daae3f5649f
Ali nadhum <alinadhum1998@gmail.com>
2025-02-24 16:56:32 +0300

    update register fun

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,4 +1,5 @@
 const asyncHandler = require("express-async-handler");
+const { UserModel } = require("../models/UserModel");
 
 
 
@@ -11,4 +12,17 @@ const asyncHandler = require("express-async-handler");
 // ==================================
 module.exports.register = asyncHandler(async(req , res) => {
     const {name , username , password , email} = req.body;
+
+    // Create new user
+  const newUser = new UserModel({
+    name,
+    username,
+    email,
+    password,
+  });
+
+  // Save user to database
+  await newUser.save();
+
+  res.status(201).json({ message: 'User registered successfully' });
 })
