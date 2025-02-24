85bf00e205bf742fa80e264513f5e7315ef8382f
Ali nadhum <alinadhum1998@gmail.com>
2025-02-24 16:07:55 +0300

    update getProducts fun

--- controllers/productController.js
+++ controllers/productController.js
@@ -15,12 +15,11 @@ module.exports.getProducts = asyncHandler(async (req, res) => {
   const page = req.query.page * 1 || 1;
   const limit = req.query.limit * 8 || 8;
   const skip = (page - 1) * limit;
-  const sortBy = req.query.sort || "createdAt";
-  const order = req.query.order === "asc" ? 1 : -1;
+  const sortBy = req.query.sort || 'createdAt';
   const products = await ProductModel.find()
     .skip(skip)
     .limit(limit)
-    .sort({ [sortBy]: order });
+    .sort(sortBy)
   const totalProducts = await ProductModel.countDocuments();
   res.status(200).json({ totalProducts, page, data: products });
 });
