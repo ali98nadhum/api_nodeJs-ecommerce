f0b94e2a8338d18211615406033ac7ef1ee72d43
Ali nadhum <alinadhum1998@gmail.com>
2025-02-24 18:04:43 +0300

    update register fun and

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const { UserModel } = require("../models/UserModel");
 const { hashPassword } = require("../helper/hashPassword");
+const { generateToken } = require("../utils/generateToken");
 
 
 
@@ -11,14 +12,13 @@ const { hashPassword } = require("../helper/hashPassword");
 // @method POST 
 // @access public
 // ==================================
-module.exports.register = asyncHandler(async(req , res) => {
-    const {name , username , password , email} = req.body;
+module.exports.register = asyncHandler(async (req, res) => {
+  const { name, username, password, email } = req.body;
 
+  // hash password
+  const hashedPassword = await hashPassword(password);
 
-    // hash password
-    const hashedPassword = await hashPassword(password);
-
-    // Create new user
+  // Create new user
   const newUser = new UserModel({
     name,
     username,
@@ -26,8 +26,17 @@ module.exports.register = asyncHandler(async(req , res) => {
     password: hashedPassword,
   });
 
+  // Generate a JWT token for a user.
+  const token = generateToken(
+    newUser.id,
+    newUser.username,
+    newUser.name,
+    newUser.role
+  );
   // Save user to database
   await newUser.save();
 
-  res.status(201).json({ message: 'User registered successfully' });
-})
+  res
+    .status(201)
+    .json({ message: "User registered successfully", token: token });
+});
