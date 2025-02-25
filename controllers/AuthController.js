e613b24c05097f6053624097c6e3357b0706d82c
Ali nadhum <alinadhum1998@gmail.com>
2025-02-25 22:31:07 +0300

    add login fun

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -40,3 +40,37 @@ module.exports.register = asyncHandler(async (req, res) => {
     .status(201)
     .json({ message: "User registered successfully", token: token });
 });
+
+
+
+// ==================================
+// @desc Login 
+// @route /api/v1/auth/login
+// @method POST
+// @access Public 
+// ==================================
+module.exports.login = asyncHandler(async(req , res) => {
+  const {email , password} = req.body;
+
+  // Check if user exists
+  const user = await UserModel.findOne({email});
+  if(!user){
+    return res.status(400).json({message: "Invalid email or password"})
+  }
+
+  // Check if password matches
+  const isPasswordMatch = await bcrypt.compare(password , user.password);
+  if(!isPasswordMatch){
+    return res.status(400).json({message: "Invalid email or password"})
+  }
+
+   // Generate a JWT token for a user.
+   const token = generateToken(
+    newUser.id,
+    newUser.username,
+    newUser.name,
+    newUser.role
+  );
+
+  res.status(200).json({message:`login_success hi ${user.username}` , token:token});
+})
