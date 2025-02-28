26e6ac217449674252637b473c11d0fa84f3bc74
Ali nadhum <alinadhum1998@gmail.com>
2025-02-28 11:57:31 +0300

    update authService file

--- utils/AuthService.js
+++ utils/AuthService.js
@@ -1,4 +1,6 @@
 const asyncHandler = require("express-async-handler");
+const jwt = require('jsonwebtoken');
+
 
 exports.protect = asyncHandler(async(req , res , next) => {
     // 1- check if token exists
@@ -9,6 +11,17 @@ exports.protect = asyncHandler(async(req , res , next) => {
 
     if(!token){
         return res.status(401).json({message: "you are not logged "});
+    }
+
+    // 2- verify token
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        req.user = decoded;
+
+        next();
+    } catch (error) {
+        return res.status(401).json({ message: "your token is invalid or expired" });
     }
 })
 
