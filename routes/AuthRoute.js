2b9104bd0e167f6f2eef8bf08617f55d63f6a429
Ali nadhum <alinadhum1998@gmail.com>
2025-02-28 12:58:24 +0300

    add changePassword fun and route

--- routes/AuthRoute.js
+++ routes/AuthRoute.js
@@ -1,11 +1,13 @@
-const { register, login } = require("../controllers/AuthController");
+const { register, login, changePassword } = require("../controllers/AuthController");
 const { registerValidator, loginValidator } = require("../utils/vaildators/AuthVaildators");
+const AuthService = require("../utils/AuthService");
 
 const router = require("express").Router();
 
 
 router.route("/register").post( registerValidator, register)
 router.route("/login").post(loginValidator , login)
+router.route("/changePassword").post(AuthService.protect,changePassword)
 
 
 
